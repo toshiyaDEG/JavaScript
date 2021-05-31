c845c478b51733489806a73b9bb2cdf6374fc8a6
SarahyLopez <arche.dsp@gmail.com>
2021-05-31 18:53:45 -0500

    Estos son los archivos html y js del Ejercicio 3

---
+++ sesion_2/ejercicio_3/ejercicio3.js
@@ -0,0 +1,33 @@
+//EJERCICIO SWITCH
+
+var day = 4;
+var text;
+
+switch (day) {
+    case 0:
+        text = "Sunday";
+        break;
+    case 1:
+        text = "Monday";
+        break;
+    case 2:
+        text = "Tuesday";
+        break;
+    case 3:
+        text = "Wednesday";
+        break;
+    case 4:
+        text = "Thursday";
+        break;
+    case 5:
+        text = "Friday";
+        break;
+    case 6:
+        text = "Saturday";
+        break;
+    default:
+        text = "Error";
+
+}
+
+//console.log(text);
