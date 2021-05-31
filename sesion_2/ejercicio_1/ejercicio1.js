28df61b69e76f7dede81a3a16223bdbdcfb74ef4
SarahyLopez <arche.dsp@gmail.com>
2021-05-31 18:50:42 -0500

    Estos son los archivos html y js del Ejercicio 1

---
+++ sesion_2/ejercicio_1/ejercicio1.js
@@ -0,0 +1,33 @@
+// EJERICIO IF
+
+var time = 13;
+var saludo;
+
+if (time < 12) {
+    saludo = "Buenos días";
+}
+
+if (time < 20) {
+    saludo = "Buenas tardes";
+}
+
+if (time >= 20) {
+    saludo = "Buenas noches";
+}
+
+
+/* console.log(saludo) */
+
+//EJERCICIO IF ELSE//
+/* var time = 24;
+var saludo;
+
+if (time >= 0 && time < 12) {
+        saludo = "Buenos días";
+} else if (time >= 12 && time < 20) {
+        saludo = "Buenas tardes";
+} else if (time >= 20 && time <= 24) {
+        saludo = "Buenas noches";
+} else {
+    saludo = "Hora desconocida";
+} */
